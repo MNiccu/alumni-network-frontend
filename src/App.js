0440c9637c9cbdd7d28e97754d1d36ec92d13c9e
MNiccu <mikkonikku@hotmail.com>
2021-10-07 00:27:28 +0300

    Unfinished. Keycloak added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Login from "./components/Login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <p>
-          Alumni Network
-        </p>
-      </header>
-    </div>
-  );
+import KeycloakService from "./services/KeycloakService";
+
+const App = () => {
+
+	return (
+		
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/">
+					<Redirect to="/login" />
+				</Route>
+				<Route path="/login" component={Login} />
+			</Switch>
+		</BrowserRouter>
+		
+	)
 }
 
 export default App;
+
+
