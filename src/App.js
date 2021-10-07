64252592d979349f68021437e6ae02dd244bb2c7
MNiccu <mikkonikku@hotmail.com>
2021-10-08 00:55:28 +0300

    Added basic calendar using react-big-calendar and date-fns

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
-import Calendar from "./components/Calendar/Calendar"
 import CreateEditPost from "./components/CreateEditPost/CreateEditPost"
 import CreateEvent from "./components/CreateEvent/CreateEvent"
 import Timeline from "./components/Timeline/Timeline"
 import GroupDetail from "./components/GroupDetail/GroupDetail"
 import GroupList from "./components/GroupList/GroupList"
 import Navigation from "./components/Navigation/Navigation"
+import CalendarComponent from "./components/Calendar/CalendarComponent";
 
 const App = () => {
 
@@ -22,6 +22,7 @@ const App = () => {
 
 				<Route path="/login" component={Login} />
 				<Route exact path="/timeline" component={Timeline} />
+				<Route exact path="/calendar" component={CalendarComponent} />
 				
 			</Switch>
 		</BrowserRouter>
