604b80d65309f543d5f68d4c5a26efc6b8b7c472
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-10-15 12:00:43 +0300

    Add routes to user and events page and add some components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
 import CreateEditPost from "./components/CreateEditPost/CreateEditPost"
-import CreateEvent from "./components/CreateEvent/CreateEvent"
+import Events from "./components/Events/Events"
 import Timeline from "./components/Timeline/Timeline"
 import GroupDetail from "./components/GroupDetail/GroupDetail"
 import GroupList from "./components/GroupList/GroupList"
 import Navigation from "./components/Navigation/Navigation"
 import CalendarComponent from "./components/Calendar/CalendarComponent";
+import User from "./components/User/User"
+import SingleEvent from "./components/Events/SingleEvent";
 
 const App = () => {
 
@@ -23,7 +25,9 @@ const App = () => {
 				<Route path="/login" component={Login} />
 				<Route exact path="/timeline" component={Timeline} />
 				<Route exact path="/calendar" component={CalendarComponent} />
-				
+				<Route exact path="/user" component={User} />
+				<Route exact path="/events" component={Events} />
+				<Route exact path="/events/:id" component={SingleEvent} />
 			</Switch>
 		</BrowserRouter>
 		
