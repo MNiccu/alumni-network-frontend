9889cfe334814775873c576716caab794092bf94
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-10-20 00:14:55 +0300

    Add redirect functionality to event cards, count of attending people and join event button

--- src/components/Events/Event.jsx
+++ src/components/Events/Event.jsx
@@ -1,36 +1,42 @@
-import { Card, Row, Col, Image } from "react-bootstrap"
-
+import { Card, Col, Button } from "react-bootstrap"
+import { useHistory } from "react-router-dom"
+import { useCallback } from "react"
 
 const Event = ({events}) => {
 
     const EventCard = ({event}) => {
 
         const start = new Date(event.startTime)
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
+        const history = useHistory()
+        const handleEventClick = useCallback(() => history.push(`events/${event.eventId}`), [history])
+        
 
         return (
-            <Col>
-                <Card className="event my-5 h-100">
-                        <img className="event-image" src={`https://avatars.dicebear.com/api/avataaars/userid.svg`} alt="user profile" fluid />
-                    <Card.Header className="">
-                        <span className="material-icons align-middle text-center">schedule</span>
-                        <span className="align-middle text-center">{start.toLocaleString('en-GB', { timeZone: 'UTC' })}</span>
-                        <Card.Text className="txt">
-                            {event.name}
-                        </Card.Text>
-                    </Card.Header>
-                    
-                </Card>
-            </Col>
-
+                <Col className="mt-5">
+                    <Card className="event mt-1 mb-0 h-100" type="button" onClick={ handleEventClick}>
+                            <img className="event-image" src={`https://avatars.dicebear.com/api/avataaars/userid.svg`} alt="user profile" />
+                        <Card.Body className="">
+                            <span className="material-icons align-middle text-center">schedule</span>
+                            <span className="align-middle text-center">{start.toLocaleString('en-GB', { timeZone: 'UTC' })}</span>
+                            <Card.Text className="txt">
+                                {event.name}
+                            </Card.Text>
+                            <Card.Text className="">
+                                {event.guestCount} is attending to this event
+                            </Card.Text>
+                            <Button size="sm" variant="outline-danger">
+                                Join this event
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
         )
     }
 
     return (
         events.map(event => {
             return (
-                <EventCard key={event.eventId} event={event} className="h-100" />
+                <EventCard key={event.eventId} event={event} className="" />
             )
         })
     )
