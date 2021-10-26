d0eb3226c2ebc0ddcfd7f2298bc3c86715bb4799
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-10-26 20:26:10 +0300

    Add update user

--- src/components/Login/LoginApi.jsx
+++ src/components/Login/LoginApi.jsx
@@ -42,5 +42,32 @@ export const LoginAPI = {
         }
         return await response.json()
       })
+      .catch(error => {
+        console.log(error)
+        return null
+      })
+  },
+
+  async updateUser(token, id, user) {
+    return fetch(`${url}${id}`, {
+      method: "PUT",
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        "Access-Control-Allow-Credentials" : true,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+      .then(async response => {
+        if(!response.ok) {
+          const { error = "Error occured while posting user to database"} = await response.json()
+          throw Error(error)
+        }
+        return await response.json()
+      })
+      .catch(error => {
+        console.log(error)
+        return null
+      })
   }
 }
