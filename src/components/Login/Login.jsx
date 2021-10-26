658fa09abfb26d636c15c6a1fa6eaeb3198ff511
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-10-26 14:08:57 +0300

    Add dispatch and GET user info in login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,33 @@
-import { Form } from "react-bootstrap"
 import { Redirect } from "react-router-dom"
+import { useDispatch } from "react-redux";
 import KeycloakService from "../../services/KeycloakService"
 import { useEffect} from "react"
-import { LoginAPI } from "./LoginApi"
-import axios from "axios"
+import { LoginAPI } from "./LoginAPI"
+import { userinfoSetAction } from "../../store/actions/userAction";
+import jwt_decode from "jwt-decode";
+
 
 const Login = (props) => {
+
+	const dispatch = useDispatch();
+
 	const getUserInfo = async () => {
 		try {
 			const token =  await KeycloakService.getToken()
-			LoginAPI.userApiFetch(token,KeycloakService.getUserId());
-
+			// console.log(token)
+			// console.log(KeycloakService.getUserId())
+			// console.log(jwt_decode(token))
+			LoginAPI.getUser(token, KeycloakService.getUserId())
+				.then(response => {
+					dispatch(userinfoSetAction(response, token))
+					// console.log(response)
+					if(response.bio === null){
+						// LoginAPI()
+						props.history.push("/moreinfo")
+					}
+					else
+						props.history.push("/timeline")					
+				})
 		}catch(e) {
 			console.log(e);
 		}
@@ -27,9 +44,13 @@ const Login = (props) => {
 
 
 	return (
-		<div className="background">
-			
-			
+		<div className="container">
+			<div className="card my-5 w-50 mx-auto border-success">
+				<div className="card-body text-center align-middle">
+					<span className="align-middle me-2">Setting up your alumni network</span>
+					<span className="spinner-border text-success align-middle" role="status"></span>
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/components/Login/LoginApi.jsx
+++ src/components/Login/LoginApi.jsx
@@ -1,21 +1,44 @@
-import KeycloakService from "../../services/KeycloakService"
-import axios from "axios"
-
-const url = "https://localhost:44344/api/User/"
-//let bearer = 'Bearer ' + KeycloakService.getToken()
-//axios.defaults.headers.common = {'Authorization': `${bearer}`}
+const url = "https://localhost:44344/api/user/"
+//const url = "https://alumninetworkbackend20211025174426.azurewebsites.net/api/topic"
 
 export const LoginAPI = {
-  userApiFetch(token, id) {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
-    getUser(id)
-  }
-}
-const getUser = async (id) => {
-  try {
-      const response = await axios.get(`${url}${id}`);
-      console.log(response.data)
-  }catch(error){
-    console.log(error)
+
+  async getUser(token, id) {
+    return fetch(`${url}${id}`, {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        "Access-Control-Allow-Origin" : "*", 
+        "Access-Control-Allow-Credentials" : true,
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(async response => {
+        if(!response.ok) {
+          const { error = "Error occured while fetching user in log in"} = await response.json()
+          throw Error(error)
+        }
+        return await response.json()
+      })
+  },
+  
+  async postUser(token, user) {
+    return fetch(`${url}`, {
+      method: "POST",
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        "Access-Control-Allow-Origin" : "*", 
+        "Access-Control-Allow-Credentials" : true,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+      .then(async response => {
+        if(!response.ok) {
+          const { error = "Error occured while posting user to database"} = await response.json()
+          throw Error(error)
+        }
+        return await response.json()
+      })
   }
 }
